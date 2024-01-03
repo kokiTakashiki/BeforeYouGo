33787ac242de8a39118399245ed85cf945f8a431
takedatakashiki <takedatakashiki@takedatakashikinoMacBook-Pro.local>
2024-01-03 22:46:29 +0900

    DateFormatterの見直し

---
+++ BeforeYouGo/BeforeYouGo/Data/FormatStyle.swift
@@ -0,0 +1,16 @@
+//
+//  FormatStyle.swift
+//  BeforeYouGo
+//
+//  Created by takedatakashiki on 2023/11/18.
+//
+
+import Foundation
+
+enum FormatMode {
+    case dayAndTime
+    case dayOnly
+    case time
+    case dayShort
+    case dayFull
+}
